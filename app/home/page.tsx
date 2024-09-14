3299d70ca99d96f9a473237298c436edf15a921b
ShaileshIshere <markvaldoz009@gmail.com>
2024-09-14 20:46:01 +0530

    created a fresh new home tab

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,13 @@
-import Home_Main_Card from "@/components/Home/Home_Main_Card";
+import Home_Main_Card_II from "@/components/Home/Home_Main_Card_II";
 import Home_NavBar from "@/components/Home/Home_NavBar";
 
 export default function Home() {
   return (
-    <div className="min-h-fit h-screen">
+    <div className="relative min-h-screen">
+      <div className="absolute top-0 left-0 w-full z-10">
         <Home_NavBar />
-        <Home_Main_Card />
+      </div>
+      <Home_Main_Card_II />
     </div>
   )
 }
